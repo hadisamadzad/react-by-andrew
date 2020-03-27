dc0f8de687baa2f8f768e3ffceb13673642b587b
Hadi Samadzad <h.samadzad@gmail.com>
2020-03-27 22:01:13 +0430

    Production optimization implemented using env and devtools.

--- expensify-app/src/app.js
+++ expensify-app/src/app.js
@@ -1,28 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import AppRouter from './routers/AppRouter';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import AppRouter from './routers/AppRouter'
 import { Provider } from 'react-redux'
-import configureStore from './store/configureStore';
+import configureStore from './store/configureStore'
 import { addExpense } from './actions/expenses'
 import { setTextFilter } from './actions/filters'
 import getVisibleExpenses from './selectors/expenses'
 import 'normalize.css/normalize.css'
-import './styles/styles.scss';
+import './styles/styles.scss'
 
-const store = configureStore();
+const store = configureStore()
 
 store.dispatch(addExpense({ description: 'Water Bill', amount: 1200, createdAt: 1391 }))
 store.dispatch(addExpense({ description: 'Gas Bill', amount: 2300, createdAt: 1392 }))
 store.dispatch(addExpense({ description: 'Power Bill', amount: 1500, createdAt: 1388 }))
 store.dispatch(addExpense({ description: 'Rent', amount: 109500, createdAt: 1377 }))
-store.dispatch(setTextFilter('water'));
+//store.dispatch(setTextFilter('water'));
+//setTimeout(() => {
+//    store.dispatch(setTextFilter('bill'));
+//}, 3000);
 
-setTimeout(() => {
-    store.dispatch(setTextFilter('bill'));
-}, 3000);
-
-const state = store.getState()
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+//const state = store.getState()
+//const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
 
 const jsx = (
     <Provider store={store}>
